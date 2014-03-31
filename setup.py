7ab2d3d460c53fe64abba232bf55345bddf335e0
twneale <twneale@gmail.com>
2014-03-30 21:09:43 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+long_description = """Basic tree node implementation.
+"""
+
+appname = "treebie"
+version = "0.00"
+
+setup(**{
+    "name": appname,
+    "version": version,
+    "packages": [
+        'tater',
+        ],
+    "author": "Thom Neale",
+    "packages": find_packages(exclude=['tests*']),
+    "author_email": "twneale@gmail.com",
+    "long_description": long_description,
+    "description": 'Basic tree nodes.',
+    "license": "MIT",
+    "url": "http://twneale.github.com/treebie/",
+    "platforms": ['any'],
+    "scripts": [
+    ]
+})
